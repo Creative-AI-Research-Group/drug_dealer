833b573353c2b7e73a208362f4cfdcd99410727b
craigv <vear2ev2.co.uk>
2021-11-08 10:54:48 +0000

    debugging w/ DD

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ class DD_signal_in:
                 # function `inWaiting()` below!
                 if (self.serDD.inWaiting() > 0):
                     # read the bytes and convert from binary array to ASCII
-                    incoming = self.serDD.read(self.serDD.inWaiting())
+                    incoming = self.serDD.read()
                     # print the incoming string without putting a new-line
                     # ('\n') automatically after every print()
                     print(incoming, end='')
