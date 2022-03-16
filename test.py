b0673cfc848675c6fe7fd89be6729f5d300a9b71
craigv <vear2ev2.co.uk>
2022-03-16 16:43:23 +0000

    added threads. Working prototype

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import serial
 
 portDD = '/dev/ttyUSB2'
 
+portBot = '/dev/ttyUSB0'
+
 
 serDD = serial.Serial(port=portDD,
                            baudrate=9600,
@@ -10,8 +12,18 @@ serDD = serial.Serial(port=portDD,
                            bytesize=serial.EIGHTBITS,
                            timeout=1
                            )
+
+serBot = serial.Serial(port=portBot,
+                           baudrate=9600,
+                           parity=serial.PARITY_NONE,
+                           stopbits=serial.STOPBITS_ONE,
+                           bytesize=serial.EIGHTBITS,
+                           timeout=1
+                           )
+
 serDD.isOpen()
+serBot.isOpen()
 
 while serDD:
     incoming = serDD.read()
-    print (incoming)
+    print (f'DD         {incoming}')
